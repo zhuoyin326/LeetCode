5e1fb3a1acd04cb715857f8bf568fc502f05518b
Joe Yin <zhuoyin.enjoy@foxmail.com>
2023-07-21 15:13:52 -0400

    replace variable window with dq

--- Queue/239_Sliding_Window_Maximum.py
+++ Queue/239_Sliding_Window_Maximum.py
@@ -6,8 +6,8 @@ Title: 239. Sliding Window Maximum
 Tag: Array, Queue, Sliding Window, Heap (Priority Queue), Monotonic Queue
 Difficulty: Hard
 
-You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
-Return the max sliding window.
+You are given an array of integers nums, there is a sliding dq of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the dq. Each time the sliding dq moves right by one position.
+Return the max sliding dq.
 
 Example 1:
 Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
@@ -40,11 +40,11 @@ class Solution:
         dq = deque()
         result = []
         
-        # Process the indices of the input list covered by the first sliding window
+        # Process the indices of the input list covered by the first sliding dq
         for i in range(k):
             # Print index i
             print("the monotonic queue before appending:", dq)
-            print("i for the first sliding window:", i)
+            print("i for the first sliding dq:", i)
             # If (a) the current element is larger than or equal to the last elememt in the monotonic queue,
             # and (b) the monotonic queue is not empty.
             while dq and nums[i] >= nums[dq[-1]]:
@@ -55,15 +55,15 @@ class Solution:
             dq.append(i)
             print("the monotonic queue after appending:", dq, "\n")
 
-        # Add the maximum number within the first sliding window to the result
+        # Add the maximum number within the first sliding dq to the result
         # Obtain the maximum number using the index of the first element in the monotonic queue 
         result.append(nums[dq[0]])
-        # Print result after the first sliding window
-        print("result after the first sliding window:", result, "\n")
+        # Print result after the first sliding dq
+        print("result after the first sliding dq:", result, "\n")
 
-        # Process the indices of the input list covered by the second sliding window and beyond
+        # Process the indices of the input list covered by the second sliding dq and beyond
         for i in range(k, len(nums)):
-            print("i for the second sliding window and beyond:", i)
+            print("i for the second sliding dq and beyond:", i)
             if dq and dq[0] == i - k:
                 # 
                 poppedLeftIndex = dq.popleft()
@@ -79,19 +79,19 @@ class Solution:
             dq.append(i)
             print("the monotonic queue after appending:", dq, "\n")
             
-            # Add the maximum number within the current sliding window to the result
+            # Add the maximum number within the current sliding dq to the result
             # Obtain the maximum number using the index of first element in the monotonic queue 
             result.append(nums[dq[0]])
             
-            # Print result after the second sliding window and beyond
-            print("result after the second sliding window and beyond:", result, "\n")
+            # Print result after the second sliding dq and beyond
+            print("result after the second sliding dq and beyond:", result, "\n")
             
         return result
     
 # Create a Solution object
 s = Solution()
 
-# Find the maximum of all elements within each sliding window
+# Find the maximum of all elements within each sliding dq
 result = s.maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3)
 
 # Print the final results
@@ -102,25 +102,25 @@ print("result:", result)
 from collections import deque
 
 class Solution2:
-    # Define a function that takes an array of integers, a window size, and returns the max sliding window
+    # Define a function that takes an array of integers, a dq size, and returns the max sliding dq
     def maxSlidingWindow2(self, nums, k):
         # Initialize an empty list to store the results
         result = []
-        # Initialize a deque to store the indices of the elements in the window
-        window = deque()
+        # Initialize a deque to store the indices of the elements in the dq
+        dq = deque()
         # Loop through the array
         for i in range(len(nums)):
-            # If the leftmost element in the window is out of range, pop it from the deque
-            if window and window[0] <= i - k:
-                window.popleft()
-            # While the deque is not empty and the current element is larger than the rightmost element in the window, pop it from the deque
-            while window and nums[i] >= nums[window[-1]]:
-                window.pop()
+            # If the leftmost element in the dq is out of range, pop it from the deque
+            if dq and dq[0] <= i - k:
+                dq.popleft()
+            # While the deque is not empty and the current element is larger than the rightmost element in the dq, pop it from the deque
+            while dq and nums[i] >= nums[dq[-1]]:
+                dq.pop()
             # Append the current index to the deque
-            window.append(i)
-            # If the window size is reached, append the leftmost element in the window to the result list
+            dq.append(i)
+            # If the dq size is reached, append the leftmost element in the dq to the result list
             if i >= k - 1:
-                result.append(nums[window[0]])
+                result.append(nums[dq[0]])
         # Return the result list
         return result
 
@@ -128,7 +128,7 @@ class Solution2:
 # Create a Solution object
 s2 = Solution2()
 
-# Find the maximum of all elements within each sliding window
+# Find the maximum of all elements within each sliding dq
 result2 = s2.maxSlidingWindow2([1,3,-1,-3,5,3,6,7], 3)
 
 # Print the final results
