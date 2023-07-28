e922e2cc40e42a2e8e17ca53d1fc2ccebe84a5ce
Joe Yin <zhuoyin.enjoy@foxmail.com>
2023-07-28 16:05:49 -0400

    correct typos

--- Graph/1514_Path_With_Maximum_Probability.py
+++ Graph/1514_Path_With_Maximum_Probability.py
@@ -52,8 +52,8 @@ class Solution:
             print("i:", i, "edge:", edge)
             print("edge[0]:", edge[0], "edge[1]:", edge[1], "succProb[i]:", succProb[i])
             # Key represents each node 
-            # Append turple as value for each node (each key in the dictionary)
-            # The turple has two values; 
+            # Append tuple as value for each node (each key in the dictionary)
+            # The tuple has two values; 
             # the first value is the neighbor node, while the second value is the probability
             graph[edge[0]].append((edge[1], succProb[i]))
             print("graph after", i, "iteration:", graph)
