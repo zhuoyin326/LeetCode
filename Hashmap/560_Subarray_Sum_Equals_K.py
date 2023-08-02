a6ac2ab5e07fac31cd0aa4f723fc1cc559352da1
Joe Yin <zhuoyin.enjoy@foxmail.com>
2023-08-02 16:02:45 -0400

    insert python code

--- Hashmap/560_Subarray_Sum_Equals_K.py
+++ Hashmap/560_Subarray_Sum_Equals_K.py
@@ -22,3 +22,38 @@ Constraints:
 -107 <= k <= 107  
     
 """
+
+def subarraySum(nums, k):
+    # Initialize count to 0. 
+    # This variable will hold the total number of subarrays that sum up to k
+    count = 0
+
+    # Initialize cumulative_sum to 0. 
+    # This variable will keep track of the cumulative sum while traversing the array
+    cumulative_sum = 0
+
+    # Initialize sum_dict dictionary to keep track of cumulative sums and their counts
+    # Key 0 is initialized with a count of 1 because sum 0 can be obtained by taking no elements
+    sum_dict = {0: 1}
+
+    # Iterate over each number in the nums array
+    for num in nums:
+        # Add the current number to cumulative_sum
+        cumulative_sum += num
+
+        # If cumulative_sum - k is in sum_dict, add its count to our answer
+        # This is because if there's a cumulative sum j such that j - k (i.e., the current cumulative_sum) exists in the array,
+        # it means that there are some subarrays ending at current index with sum k
+        if cumulative_sum - k in sum_dict:
+            count += sum_dict[cumulative_sum - k]
+
+        # If cumulative_sum is already a key in sum_dict, increment its count
+        # Otherwise, add cumulative_sum to sum_dict with a count of 1
+        if cumulative_sum in sum_dict:
+            sum_dict[cumulative_sum] += 1
+        else:
+            sum_dict[cumulative_sum] = 1
+
+    # After traversing the entire array, return count
+    # This will be the total number of contiguous subarrays that sum up to k
+    return count
