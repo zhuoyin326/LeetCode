a90a7c281899eda0bcfd1c5d8d7cd2c566a4b726
Joe Yin <zhuoyin.enjoy@foxmail.com>
2023-08-19 15:55:28 -0400

    edit code and comments

--- Tree/segment tree.py
+++ Tree/segment tree.py
@@ -1,11 +1,9 @@
-"""
-Building the Segment Tree
-"""
-
+# Building the Segment Tree
 def build_tree(arr, tree, start, end, treeIndex):
     # Base case: If start equals end, it means we're at a leaf node
     if start == end:
-        tree[treeIndex] = arr[start] # Set the value at this leaf node
+        # Set the value at this leaf node
+        tree[treeIndex] = arr[start]
     else:
         # Calculate the mid-point to divide the array into two halves
         mid = (start + end) // 2
@@ -19,9 +17,8 @@ def build_tree(arr, tree, start, end, treeIndex):
         # The parent node's value is the sum of its children nodes
         tree[treeIndex] = tree[2*treeIndex + 1] + tree[2*treeIndex + 2]
 
-"""
-Querying the Segment Tree
-"""
+
+# Querying the Segment Tree
 def query_tree(tree, start, end, l, r, treeIndex):
     # No overlap case: if the queried range is outside the current segment
     if r < start or l > end: 
@@ -44,40 +41,51 @@ def query_tree(tree, start, end, l, r, treeIndex):
     return left_sum + right_sum
 
 
-"""
-Updating the Segment Tree
-"""
+# Updating the Segment Tree
 def update_tree(arr, tree, start, end, treeIndex, arrIndex, value):
     # Base case: If we're at the exact treeIndex to be updated
     if start == end:
-        arr[arrIndex] = value # Update the original array
-        tree[treeIndex] = value # Update the segment tree node
+        # Update the original array
+        arr[arrIndex] = value
+        # Update the segment tree node
+        tree[treeIndex] = value
     else:
         # Calculate the mid-point
         mid = (start + end) // 2
         
-        # If the update treeIndex is in the left half, recursively update the left child
+        # If the update treeIndex is in the left half
         if start <= arrIndex <= mid:
+            # Recursively update the left child
             update_tree(arr, tree, start, mid, 2*treeIndex + 1, arrIndex, value)
-        # Otherwise, recursively update the right child
+        # Otherwise
         else:
+            # Recursively update the right child
             update_tree(arr, tree, mid+1, end, 2*treeIndex + 2, arrIndex, value)
         
         # After updating child(ren), update the current node
         tree[treeIndex] = tree[2*treeIndex + 1] + tree[2*treeIndex + 2]
 
 
-"""
-Example Usage
-"""
-arr = [1, 2, 3, 4, 5] # Input array
-n = len(arr) # Size of the input array
-tree = [0] * (4 * n) # Segment tree, size 4n is a safe upper bound
-build_tree(arr, tree, 0, n-1, 0) # Build the segment tree from the array
+# Example Usage
 
-print(query_tree(tree, 0, n-1, 1, 3, 0)) # Query the sum from treeIndex 1 to 3. Expected output: 9
+# Input array
+arr = [1, 2, 3, 4, 5] 
 
-update_tree(arr, tree, 0, n-1, 0, 2, 6) # Update the element at treeIndex 2 to 6
+# Size of the input array
+n = len(arr) 
 
-print(query_tree(tree, 0, n-1, 1, 3, 0)) # Query the sum from treeIndex 1 to 3 again. Expected output: 12
+# Segment tree, size 4n is a safe upper bound
+tree = [0] * (4 * n) 
+
+# Build the segment tree from the array
+build_tree(arr, tree, 0, n-1, 0) 
+
+# Query the sum from treeIndex 1 to 3. Expected output: 9
+print(query_tree(tree, 0, n-1, 1, 3, 0)) 
+
+# Update the element at treeIndex 2 to 6
+update_tree(arr, tree, 0, n-1, 0, 2, 6) 
+
+# Query the sum from treeIndex 1 to 3 again. Expected output: 12
+print(query_tree(tree, 0, n-1, 1, 3, 0)) 
 
