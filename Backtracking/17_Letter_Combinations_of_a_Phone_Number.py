9314760b37435e4fe50ef2282bbc6537154ef0a1
Joe Yin <zhuoyin.enjoy@foxmail.com>
2023-12-15 16:36:42 -0500

    update comments

--- Backtracking/17_Letter_Combinations_of_a_Phone_Number.py
+++ Backtracking/17_Letter_Combinations_of_a_Phone_Number.py
@@ -38,26 +38,26 @@ class Solution:
         if len(digits) == 0:
             return []
 
-        # Map all the digits to their corresponding num2letters
+        # Map all the numbers to their corresponding letters
         num2letters = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl",
                    "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
 
         def backtrack(index, currString):
-            # If the currString is the same length as digits, we have a complete combination
+            # If the current string is the same length as digits, we have a complete combination
             if len(currString) == len(digits):
                 combinations.append("".join(currString))
                 return  # Backtrack
 
-            # Get the num2letters that the current digit maps to, and loop through them
+            # Get the letters that the current digit maps to, and loop through them
             for letter in num2letters[digits[index]]:
-                # Add the letter to our current currString
+                # Add the letter to our current string
                 currString.append(letter)
                 # Move on to the next digit
                 backtrack(index + 1, currString)
                 # Backtrack by removing the letter before moving onto the next
                 currString.pop()
 
-        # Initiate backtracking with an empty currString and starting index of 0
+        # Initiate backtracking with an empty string and starting index of 0
         combinations = []
         backtrack(0, [])
         return combinations
