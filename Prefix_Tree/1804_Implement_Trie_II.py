6aa0170c74ac20fa51d31eef15d181db38dd6450
Joe Yin <zhuoyin.enjoy@foxmail.com>
2024-02-29 17:47:06 -0500

    update code

--- Prefix_Tree/1804_Implement_Trie_II.py
+++ Prefix_Tree/1804_Implement_Trie_II.py
@@ -40,49 +40,71 @@ At most 3 * 104 calls in total will be made to insert, countWordsEqualTo, countW
 It is guaranteed that for any function call to erase, the string word will exist in the trie.
 """
 
-
 class TrieNode:
+    # Each TrieNode contains a dictionary to hold its children and variables to count words and prefixes
     def __init__(self):
-        self.links = [None] * 26
-        self.words_ending_here = 0
-        self.words_starting_here = 0
+        # Map character to TrieNode
+        self.children = {}
+        # Count of words ending at this node
+        self.endOfWord = 0
+        # Count of words having this node as prefix
+        self.prefixCount = 0
 
 class Trie:
+    # Trie data structure with root node initialization
     def __init__(self):
         self.root = TrieNode()
-
+    
     def insert(self, word: str) -> None:
+        # Insert a word into the trie
         node = self.root
-        for w in word:
-            char_index = ord(w) - ord('a')
-            if not node.links[char_index]:
-                node.links[char_index] = TrieNode()
-            node = node.links[char_index]
-            node.words_starting_here += 1
-        node.words_ending_here += 1
-
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+            # Increment prefix count for the current node
+            node.prefixCount += 1
+        # Mark the end of a word
+        node.endOfWord += 1
+    
     def countWordsEqualTo(self, word: str) -> int:
+        # Return the number of times a word is inserted
         node = self.root
-        for w in word:
-            char_index = ord(w) - ord('a')
-            if not node.links[char_index]:
+        for char in word:
+            if char not in node.children:
+                # Word not found
                 return 0
-            node = node.links[char_index]
-        return node.words_ending_here
-
+            node = node.children[char]
+        return node.endOfWord
+    
     def countWordsStartingWith(self, prefix: str) -> int:
+        # Return the number of words having the given prefix
         node = self.root
-        for w in prefix:
-            char_index = ord(w) - ord('a')
-            if not node.links[char_index]:
+        for char in prefix:
+            if char not in node.children:
+                # Prefix not found
                 return 0
-            node = node.links[char_index]
-        return node.words_starting_here
-
+            node = node.children[char]
+        return node.prefixCount
+    
     def erase(self, word: str) -> None:
+        # Remove a word from the trie
         node = self.root
-        for w in word:
-            char_index = ord(w) - ord('a')
-            node = node.links[char_index]
-            node.words_starting_here -= 1
-        node.words_ending_here -= 1
+        # To keep track of nodes visited
+        stack = []
+        for char in word:
+            if char not in node.children:
+                # Word not found, nothing to erase
+                return
+            # Add current node and char to stack
+            stack.append((node, char))  
+            node = node.children[char]
+        # Only decrement if word exists
+        if node.endOfWord > 0:
+            node.endOfWord -= 1
+            # Walk back and update prefixCounts
+            for node, char in reversed(stack):
+                node.children[char].prefixCount -= 1
+                if node.children[char].prefixCount == 0:
+                    # Remove the node if no longer needed
+                    del node.children[char]
