c3e109729aae07fd372ee6d0bad29c59e22fe7d5
Joe Yin <zhuoyin.enjoy@foxmail.com>
2024-06-25 17:25:41 -0400

    Simplify code for monotonic queue and stack.

--- Stack/monotonic_decreasing_stack_and_queue.py
+++ Stack/monotonic_decreasing_stack_and_queue.py
@@ -4,34 +4,27 @@ A monotonic decreasing queue is a data structure where the elements are maintain
 """
 
 from collections import deque
-
-class MonotonicDecreasingQueue:
-    def __init__(self):
-        self.queue = deque()
-    
-    def push(self, value):
-        while self.queue and self.queue[-1] < value:
-            self.queue.pop()
-        self.queue.append(value)
-    
-    def pop(self):
-        self.queue.popleft()
-    
-    def front(self):
-        return self.queue[0] if self.queue else None
-
-    def __str__(self):
-        return str(list(self.queue))
+def monotonicDecreasingQueue(list):
+    queue = deque()
+    for x in list:
+        # While the deque is not empty and the last element is smaller than x
+        while queue and queue[-1] < x:
+            # Remove the last element
+            queue.pop()
+        # Append x to the deque
+        queue.append(x)
+    return queue
 
 # Example usage
-md_queue = MonotonicDecreasingQueue()
-md_queue.push(5)
-md_queue.push(3)
-md_queue.push(4)  # This will remove 3 before adding 4
-md_queue.push(2)
-print(md_queue)   # Output: [5, 4, 2]
-md_queue.pop()
-print(md_queue)   # Output: [4, 2]
+list = [7, 5, 3, 1, 2, 4]
+queue = monotonicDecreasingQueue(list)
+print(queue)
+# Pop an element from the current queue
+queue.popleft()
+print(queue)
+# Pop another element from the current queue
+queue.popleft()
+print(queue)
 
 
 """
@@ -39,32 +32,27 @@ Monotonic Decreasing Stack
 A monotonic decreasing stack is a data structure where the elements are maintained in decreasing order from the top to the bottom of the stack. This means that each element in the stack is smaller than or equal to the previous element.
 """
 
-class MonotonicDecreasingStack:
-    def __init__(self):
-        self.stack = []
-    
-    def push(self, value):
-        while self.stack and self.stack[-1] < value:
-            self.stack.pop()
-        self.stack.append(value)
-    
-    def pop(self):
-        if self.stack:
-            self.stack.pop()
-    
-    def top(self):
-        return self.stack[-1] if self.stack else None
+def monotonicDecreasingStack(list):
+   stack = []
+   for x in list:
+       # While the stack is not empty and the last element is smaller than x
+       while stack and stack[-1] < x:
+           # Remove the last element
+           stack.pop()
+       # Append x to the stack
+       stack.append(x)
+   return stack
 
-    def __str__(self):
-        return str(self.stack)
 
 # Example usage
-md_stack = MonotonicDecreasingStack()
-md_stack.push(5)
-md_stack.push(3)
-md_stack.push(4)  # This will remove 3 before adding 4
-md_stack.push(2)
-print(md_stack)   # Output: [5, 4, 2]
-md_stack.pop()
-print(md_stack)   # Output: [5, 4]
+list = [7, 5, 3, 1, 2, 4]
+stack = monotonicDecreasingStack(list)
+print(stack)
+# Pop an element from the current stack
+stack.pop()
+print(stack)
+# Pop another element from the current stack
+stack.pop()
+print(stack)
+
 
